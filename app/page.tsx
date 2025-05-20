47ca78e34d2c397dba22c2fdd0d9315c223616f9
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-05-20 23:02:31 +0300

    i dont know whats wrong

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
-export default function Home() {
-  return (
-    
-  );
-}
+ export default function Home() {
+    return(
+        <div>
+            <div>Home</div>
+            <Button variant="outline">Bascket</Button>
+        </div>
+    )
+ }
